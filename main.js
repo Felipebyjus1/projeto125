ebf0b77fc1d39f6f848cb9a235df44744ed4d241
FELIPE DASIL <125500685+Felipebyjus1@users.noreply.github.com>
2023-11-27 17:43:53 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ noseY = 0
 fontSize = 0
 leftWristX = 0
 rightWristX = 0
+confianceNose = 0
 
 function setup(){
     canvas = createCanvas(600, 600)
@@ -26,14 +27,17 @@ function gotPoses(results){
      noseY = results[0].pose.nose.y
      leftWristX = results[0].pose.leftWrist.x
      rightWristX = results[0].pose.rightWrist.x
+     confianceNose = results[0].pose.nose.x
      fontSize = floor(leftWristX - rightWristX)
      document.getElementById("resultado").innerHTML =  + fontSize
     }
 }
 
 function draw(){
-    background("black")
-    textSize(25)
-    fill(white)
-    text("Estou sem criatividade então esse texto fica aqui para concluir o projeto :D", 200, 200)
+    if(confianceNose > 0.5){
+    background("green")
+    textSize(fontSize)
+    fill("white")
+    text(":D", noseX, noseY)
+    }
 }
